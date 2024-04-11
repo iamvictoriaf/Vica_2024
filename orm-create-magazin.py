278f4826eec56aee7bb61f49cdde4f2682bb3b2d
ChumaTV <chumatv@gmail.com>
2024-04-11 21:32:43 +0300

    добавила класс item

--- orm-create-magazin.py
+++ orm-create-magazin.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Table, Index, Integer, String, \
     Column, Text, DateTime, Boolean, PrimaryKeyConstraint, \
-    UniqueConstraint, ForeignKeyConstraint, create_engine
+    UniqueConstraint, ForeignKeyConstraint, create_engine, Numeric
 
 from sqlalchemy.orm import declarative_base, relationship, Session
 from datetime import datetime
@@ -19,3 +19,12 @@ class Customer(Base):
     email = Column(String(200), nullable=False)
     created_on = Column(DateTime, default=datetime.now)
     update_on = Column(DateTime, default=datetime.now, onupdate=datetime.now())
+    orders = relationship('Order')
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10,2), nullable=False)
+    seling_price = Column(Numeric(10, 2), nullable=False)
+    count = Column(Integer)
